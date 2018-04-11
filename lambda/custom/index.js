0716ab98bfacdb80be86860f0b6384124100cd1d
Patil Omkar <PatilOmkar@JohnDeere.com>
2018-04-11 19:02:07 +0530

    1st commit
    1st commit

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -17,11 +17,11 @@ const Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = undefined;
+const APP_ID = 'amzn1.ask.skill.1d4d0e87-b937-4119-8a82-a3a8019c4ce8';
 
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = "Here's your fact: ";
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const SKILL_NAME = 'QSharp';
+const GET_FACT_MESSAGE = "Here you go: ";
+const HELP_MESSAGE = 'You can say how many total q-notes created on a given day?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
@@ -29,19 +29,26 @@ const STOP_MESSAGE = 'Goodbye!';
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
 //=========================================================================================================================================
 const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.'
+    'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!',
+       'Ask Minal',
+    'Ask Minal Always!'
 ];
 
 //=========================================================================================================================================
